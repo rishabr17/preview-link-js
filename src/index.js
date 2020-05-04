29746155d98c83c6fe36502b0f33aef3fc9c24b5
Rishab <rishabravikumar@yahoo.com>
2020-05-04 17:17:47 -0400

    Setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class PreviewLink extends Component {
+
+    render () {
+
+        return (
+            <div>Preview of Link</div>
+        )
+
+    }
+
+}
